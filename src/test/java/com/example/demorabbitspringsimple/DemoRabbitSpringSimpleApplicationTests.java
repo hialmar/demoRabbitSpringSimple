fe5441cdbfe2cd548e99b532ee26436fe079aa14
Patrice Torguet <patrice.torguet@irit.fr>
2023-06-07 14:54:04 +0200

    Initial commit

---
+++ src/test/java/com/example/demorabbitspringsimple/DemoRabbitSpringSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demorabbitspringsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRabbitSpringSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
