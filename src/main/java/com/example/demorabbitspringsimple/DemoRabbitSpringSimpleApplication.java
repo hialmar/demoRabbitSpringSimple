9df4ee3c5f1f43c08bcdda9baba6ac2dcf3cc1bd
Patrice Torguet <patrice.torguet@irit.fr>
2023-06-12 10:51:55 +0200

    Ajout de l'exemple.

--- src/main/java/com/example/demorabbitspringsimple/DemoRabbitSpringSimpleApplication.java
+++ src/main/java/com/example/demorabbitspringsimple/DemoRabbitSpringSimpleApplication.java
@@ -1,13 +1,27 @@
 package com.example.demorabbitspringsimple;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoRabbitSpringSimpleApplication {
+    public static void main(String[] args) { SpringApplication.run(DemoRabbitSpringSimpleApplication.class, args); }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoRabbitSpringSimpleApplication.class, args);
-	}
+    @Bean
+    public ApplicationRunner runner(AmqpTemplate template) {
+        return args -> template.convertAndSend("myqueue", "foo"); // foo est la routing key
+    }
 
+    @Bean
+    public Queue myQueue() { return new Queue("myqueue"); }
+
+    @RabbitListener(queues = "myqueue")
+    public void listen(String in) {
+        System.out.println(in);
+    }
 }
--- src/test/java/com/example/demorabbitspringsimple/DemoRabbitSpringSimpleApplicationTests.java
+++ src/test/java/com/example/demorabbitspringsimple/DemoRabbitSpringSimpleApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoRabbitSpringSimpleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
